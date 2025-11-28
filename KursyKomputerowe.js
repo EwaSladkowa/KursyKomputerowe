9a349dc9f7e6bb298de779b39ea806988f84b745
EwaSladkowa <sladkowayewa126@gmail.com>
2025-11-28 12:43:55 +0100

    .

--- KursyKomputerowe.js
+++ KursyKomputerowe.js
@@ -1,10 +1,10 @@
 darkMode = document.getElementById("dark-mode");
 darkMode.addEventListener("click", () => {
-  // if (darkMode.innerText = "Change to light mode" = true){
-  //     darkMode.innerText = "Change to dark mode";
-  // }else{
-  //     darkMode.innerText = "Change to light mode";
-  // }
+  if (darkMode.innerText === "Change to dark mode") {
+    darkMode.innerText = "Change to light mode";
+  } else{
+    darkMode.innerText = "Change to dark mode";
+  }
   const div1 = document.getElementsByClassName("div1")[0];
   div1.classList.toggle("darkMode");
 });
