1adfcbf14de809ad6fac22a0f9c17608d0baa785
EwaSladkowa <sladkowayewa126@gmail.com>
2025-12-05 10:28:18 +0100

    .

--- KursyKomputerowe.js
+++ KursyKomputerowe.js
@@ -1,10 +1,34 @@
 darkMode = document.getElementById("dark-mode");
+let div1 = document.getElementsByClassName("div1")[0];
 darkMode.addEventListener("click", () => {
-  if (darkMode.innerText === "Change to dark mode") {
+  Mode();
+  div1.classList.toggle("darkMode");
+  text();
+});
+
+if (window.localStorage.getItem("Darkmode") == undefined) {
+  window.localStorage.setItem("Darkmode", "false");
+}
+text();
+function Mode() {
+  let mode = window.localStorage.getItem("Darkmode");
+  if (mode == "true") {
+    mode = "false";
+    window.localStorage.setItem("Darkmode", mode);
+  } else {
+    mode = "true";
+    window.localStorage.setItem("Darkmode", mode);
+  }
+}
+if (window.localStorage.getItem('Darkmode') == 'false') {
+  div1.classList.remove("darkMode");
+}else{
+  div1.classList.add("darkMode");
+}
+function text(){
+  if (window.localStorage.getItem('Darkmode') == 'true') {
     darkMode.innerText = "Change to light mode";
-  } else{
+  } else {
     darkMode.innerText = "Change to dark mode";
   }
-  const div1 = document.getElementsByClassName("div1")[0];
-  div1.classList.toggle("darkMode");
-});
+}
